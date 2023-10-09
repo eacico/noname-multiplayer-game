612e0f4bce0d73bd9e659121be61569c9c50f25f
eacico <estebancicovich@gmail.com>
2023-10-09 17:00:51 -0300

    Separo nivel en escena propia

---
+++ src/game/levels/LevelDefault.gd
@@ -0,0 +1,28 @@
+extends Node
+class_name GameLevel
+
+
+
+# Regresa al menu principal
+signal return_requested()
+# Reinicia el nivel
+signal restart_requested()
+# Inicia el siguiente nivel
+signal next_level_requested()
+
+
+func _ready() -> void:
+	randomize()
+
+
+# Funciones que hacen de interfaz para las señales
+func _on_level_won() -> void:
+	emit_signal("next_level_requested")
+
+
+func _on_return_requested() -> void:
+	emit_signal("return_requested")
+
+
+func _on_restart_requested() -> void:
+	emit_signal("restart_requested")
