ee73b0849e2f28965a5d07a3808ac7f9f891d5f8
eacico <estebancicovich@gmail.com>
2023-10-24 23:52:59 -0300

    Apertura de portal deslizable con movimiento

--- src/game/entities/reactives/SliderGate.gd
+++ src/game/entities/reactives/SliderGate.gd
@@ -6,10 +6,18 @@ export (Array, NodePath) var switches
 onready var state_machine = $StateMachine
 onready var gate = $Gate
 
+var gate_speed: float = 10
+var target_position: float
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_initialize()
 	slide_gate_closed()
+
+func _physics_process(delta: float) -> void:
+	if target_position:
+		gate.position.x = lerp(gate.position.x, target_position, gate_speed * delta)
 
 func _initialize() -> void:
 	for i in range(switches.size()):
@@ -24,10 +32,11 @@ func _on_finished(next_state_name):
 		slide_gate_closed()
 
 func slide_gate_open() -> void:
-	gate.position.x = 47
+	target_position = 47
 	
 func slide_gate_closed() -> void:
-	gate.position.x = -47
+	target_position = -47
 
 func _play_animation(animation: String) -> void:
 	pass
+
