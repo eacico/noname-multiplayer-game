3948559b5b1164f147c61b9e3cdf8018a7c85427
eacico <estebancicovich@gmail.com>
2023-11-03 10:25:49 -0300

    _Puzzle1: Agregar seccion cruzada al pipe del switch

--- src/screens/MainMenu.gd
+++ src/screens/MainMenu.gd
@@ -3,16 +3,17 @@ extends Node
 export (PackedScene) var level_manager_scene: PackedScene
 #export (Texture) var mouse_cursor: Texture
 
-var start_on_test_scene: bool = false
-var level_tutorial_01_id = 1
-var level_01_id = 5
+export (bool) var start_on_test_scene: bool = false
+export (int) var level_to_test = 6
+export (int) var level_tutorial_01_id = 1
+export (int) var level_01_id = 5
 
 func _ready() -> void:
 	if SceneSwitcher.get_param("start_on_test_scene") != null:
 		start_on_test_scene = SceneSwitcher.get_param("start_on_test_scene")
 	#Input.set_custom_mouse_cursor(mouse_cursor)
 	if start_on_test_scene:
-		SceneSwitcher.change_scene(level_manager_scene.resource_path, {"level": 0})
+		SceneSwitcher.change_scene(level_manager_scene.resource_path, {"level": level_to_test})
 
 
 
