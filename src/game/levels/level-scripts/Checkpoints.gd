07e8e4dc8a988f077c7070c16d6fb4e1fa45f8b7
eacico <estebancicovich@gmail.com>
2023-12-15 17:10:05 -0300

    primera parte de level4

--- src/game/levels/level-scripts/Checkpoints.gd
+++ src/game/levels/level-scripts/Checkpoints.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var p_1 = $TriggerCheckpoint1/P1
 
 func checkpoint_reached(player, checkpoint_id):
+	print("Checkpoint %s reached" % [checkpoint_id])
 	GameState.checkpoint = [
 		get_node("TriggerCheckpoint%s/P1" % [checkpoint_id]).global_position,
 		get_node("TriggerCheckpoint%s/P2" % [checkpoint_id]).global_position
